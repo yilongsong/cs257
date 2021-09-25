ba14fce5a05b98f23be4f728acd3daea953742c6
yilongsong <60498408+yilongsong@users.noreply.github.com>
2021-09-25 10:31:41 -0500

    tests debugged

--- books/booksdatasourcetest.py
+++ books/booksdatasourcetest.py
@@ -31,10 +31,10 @@ import booksdatasource
 import unittest
 
 class BooksDataSourceTester(unittest.TestCase):
-    def setUp():
-        self.books_data_source = booksdatasource.BooksDataSource(test.csv)
+    def setUp(self):
+        self.books_data_source = booksdatasource.BooksDataSource("test.csv")
         
-    def tearDown():
+    def tearDown(self):
         pass
         
     #Tests for authors
@@ -43,27 +43,30 @@ class BooksDataSourceTester(unittest.TestCase):
         authorGivennameList = ["Ann", "Charlotte", "Agatha", "Sinclair", "Tommy", "Laurence", "Connie"]
         authorSurnameList = ["Brontë", "Brontë", "Christie", "Lewis", "Orange", "Sterne", "Willis"]
         
-        for i in range(len(res)):
-            assertEqual(res[i].surname, authorSurnameList[i])
-            assertEqual(res[i].given_name, authorGivennameList[i])
+        for i in range(len(authorGivennameList)):
+            self.assertNotEqual(len(res), 0)
+            self.assertEqual(res[i].surname, authorSurnameList[i])
+            self.assertEqual(res[i].given_name, authorGivennameList[i])
             
     def test_authors_general(self):
         res = self.books_data_source.authors("i")
         authorGivennameList = ["Agatha", "Sinclair", "Connie"]
         authorSurnameList = ["Christie", "Lewis", "Willis"]
         
-        for i in range(len(res)):
-            assertEqual(res[i].surname, authorSurnameList[i])
-            assertEqual(res[i].given_name, authorGivennameList[i])
+        for i in range(len(authorGivennameList)):
+            self.assertNotEqual(len(res), 0)
+            self.assertEqual(res[i].surname, authorSurnameList[i])
+            self.assertEqual(res[i].given_name, authorGivennameList[i])
             
     def test_authors_case(self):
         res = self.books_data_source.authors("RON")
         authorGivennameList = ["Ann", "Charlotte"]
         authorSurnameList = ["Brontë", "Brontë"]
         
-        for i in range(len(res)):
-            assertEqual(res[i].surname, authorSurnameList[i])
-            assertEqual(res[i].given_name, authorGivennameList[i])
+        for i in range(len(authorGivennameList)):
+            self.assertNotEqual(len(res), 0)
+            self.assertEqual(res[i].surname, authorSurnameList[i])
+            self.assertEqual(res[i].given_name, authorGivennameList[i])
             
     
     #Test for books
@@ -72,56 +75,68 @@ class BooksDataSourceTester(unittest.TestCase):
         res2 = self.books_data_source.books(search_text=None, sort_by='abcd')
         bookTitleList = ["All Clear", "Blackout", "Elmer Gantry", "Jane Eyre", "Main Street", "Murder on the Orient Express","The Life and Opinions of Tristram Shandy, Gentleman", "The Tenant of Wildfell Hall", "There, There", "Villette"]
         
-        for i in range(len(res)):
-            assertEqual(res1[i].title, bookTitleList[i])
-            assertEqual(res2[i].title, bookTitleList[i])
+        for i in range(len(bookTitleList)):
+            self.assertNotEqual(len(res1), 0)
+            self.assertNotEqual(len(res2), 0)
+            self.assertEqual(res1[i].title, bookTitleList[i])
+            self.assertEqual(res2[i].title, bookTitleList[i])
             
     def test_books_general(self):
         res = self.books_data_source.books(search_text = 'th')
         bookTitleList = ["The Life and Opinions of Tristram Shandy, Gentleman", "The Tenant of Wildfell Hall", "There, There"]
         
-        for i in range(len(res)):
-            assertEqual(res[i].title, bookTitleList[i])
+        for i in range(len(bookTitleList)):
+            self.assertNotEqual(len(res), 0)
+            self.assertEqual(res[i].title, bookTitleList[i])
             
     def test_books_case(self):
         res = self.books_data_source.books(search_text = 'TH')
         bookTitleList = ["The Life and Opinions of Tristram Shandy, Gentleman", "The Tenant of Wildfell Hall", "There, There"]
         
-        for i in range(len(res)):
-            assertEqual(res[i].title, bookTitleList[i])
+        for i in range(len(bookTitleList)):
+            self.assertNotEqual(len(res), 0)
+            self.assertEqual(res[i].title, bookTitleList[i])
         
     def test_books_year(self):
         res = self.books_data_source.books(search_text=None, sort_by='year')
         bookTitleList = ["The Life and Opinions of Tristram Shandy, Gentleman", "Jane Eyre", "The Tenant of Wildfell Hall", "Villette", "Main Street", "Elmer Gantry", "Murder on the Orient Express", "All Clear", "Blackout", "There, There"]
         
-        for i in range(len(res)):
-            assertEqual(res[i].title, bookTitleList[i])
+        for i in range(len(bookTitleList)):
+            self.assertNotEqual(len(res), 0)
+            self.assertEqual(res[i].title, bookTitleList[i])
             
     #Test for books_between_years
     def test_books_between_years_both_none(self):
         res = self.books_data_source.books_between_years(start_year=None, end_year=None)
         bookTitleList = ["The Life and Opinions of Tristram Shandy, Gentleman", "Jane Eyre", "The Tenant of Wildfell Hall", "Villette", "Main Street", "Elmer Gantry", "Murder on the Orient Express", "All Clear", "Blackout", "There, There"]
         
-        for i in range(len(res)):
-            assertEqual(res[i].title, bookTitleList[i])
+        for i in range(len(bookTitleList)):
+            self.assertNotEqual(len(res), 0)
+            self.assertEqual(res[i].title, bookTitleList[i])
     
     def test_books_between_years_general(self):
         res = self.books_data_source.books_between_years(start_year=1920, end_year = 1940)
         bookTitleList = ["Main Street", "Elmer Gantry", "Murder on the Orient Express"]
         
-        for i in range(len(res)):
-            assertEqual(res[i].title, bookTitleList[i])
+        for i in range(len(bookTitleList)):
+            self.assertNotEqual(len(res), 0)
+            self.assertEqual(res[i].title, bookTitleList[i])
             
     def test_books_between_years_start_year_none(self):
         res = self.books_data_source.books_between_years(start_year=None, end_year=1900)
         bookTitleList = ["The Life and Opinions of Tristram Shandy, Gentleman", "Jane Eyre", "The Tenant of Wildfell Hall", "Villette"]
         
-        for i in range(len(res)):
-            assertEqual(res[i].title, bookTitleList[i])
+        for i in range(len(bookTitleList)):
+            self.assertNotEqual(len(res), 0)
+            self.assertEqual(res[i].title, bookTitleList[i])
             
     def test_books_between_years_end_year_none(self):
         res = self.books_data_source.books_between_years(start_year=1900, end_year=None)
         bookTitleList = ["Main Street", "Elmer Gantry", "Murder on the Orient Express", "All Clear", "Blackout", "There, There"]
         
-        for i in range(len(res)):
-            assertEqual(res[i].title, bookTitleList[i])
+        for i in range(len(bookTitleList)):
+            self.assertNotEqual(len(res), 0)
+            self.assertEqual(res[i].title, bookTitleList[i])
+
+if __name__ == '__main__':
+    unittest.main()
