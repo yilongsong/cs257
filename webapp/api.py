76611593282c8a8f5cdad07597a134e6f66ceae3
Yilong Song <songyilong18@gmail.com>
2021-11-14 15:41:17 -0600

    ready for pie-chart implementation

--- webapp/api.py
+++ webapp/api.py
@@ -16,6 +16,56 @@ def get_connection():
                             user=config.user,
                             password=config.password)
 
+@api.route('/states/')
+def get_states():
+    query ='''SELECT state.id, state.state, state.state_po
+            FROM state
+            ORDER BY state.state;'''
+    state_list = []
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute(query)
+        for row in cursor:
+            state = {'state_id':row[0], 'state_name':row[1].lower().title(), "state_po":row[2]}
+            state_list.append(state)
+        cursor.close()
+        connection.close()
+    except Exception as e:
+        print(e, file=sys.stderr)
+    
+    return json.dumps(state_list)
+
+@api.route('/state-year/state=<state>/year=<year>/')
+def get_state_year_data(state, year):
+    search_string_state = "'%" + state.upper()[1:-1] + "%'"
+    search_string_year = year
+    query = '''SELECT candidate.id, candidate.name, election.votes_received,
+                election.votes_total
+                FROM candidate, election, state
+                WHERE candidate.id = election.candidate_id
+                    AND election.state_id = state.id
+                    AND state.state like %s
+                    AND election.year = %s
+                ORDER BY election.votes_received DESC;
+    '''
+    candidate_list = []
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute(query % (search_string_state, search_string_year))
+        for row in cursor:
+            candidate = {'candidate_id':row[0], 'candidate_name':row[1].lower().title(),
+                        'votes_received':row[2], 'votes_total':row[3]}
+            candidate_list.append(candidate)
+        cursor.close()
+        connection.close()
+    except Exception as e:
+        print(e, file=sys.stderr)
+    
+    return json.dumps(candidate_list)
+    
+
 @api.route('/candidate/<candidate_name>')
 def get_candidate_election_history(candidate_name):
     search_string = '%' + candidate_name.upper() + '%'
@@ -27,17 +77,6 @@ def get_candidate_election_history(candidate_name):
                     AND c1.id != c2.id
                 ORDER BY c1.name, e1.year DESC;'''
 
-    '''
-    For testing:
-    SELECT DISTINCT c1.name, c1.party, e1.year, state.state, e1.votes_received,
-        c2.name, e2.votes_received
-    FROM candidate c1, candidate c2, election e1, election e2, state
-    WHERE c1.id = e1.candidate_id AND c1.name like '%ALAN%' AND e1.state_id = state.id
-        AND e1.year = e2.year AND e1.state_id = e2.state_id AND c2.id = e2.candidate_id
-        AND c1.id != c2.id
-    ORDER BY e1.year DESC;
-    '''
-
     candidate_list = []
     try:
         connection = get_connection()
@@ -46,7 +85,7 @@ def get_candidate_election_history(candidate_name):
         for row in cursor:
             candidate = {'candidate_name':row[0].lower().title(), 
                          'party':row[1].lower().title(), 'year':row[2],
-                         'state':row[3], 'votes_received':row[4],
+                         'state':row[3].lower().title(), 'votes_received':row[4],
                          'other_candidate_name':row[5].lower().title(),
                          'other_candidate_votes_received':row[6]}
             candidate_list.append(candidate)
--- webapp/static/elections.js
+++ webapp/static/elections.js
@@ -7,10 +7,12 @@
 window.onload = initialize;
 
 function initialize() {
-
-    let element = document.getElementById('search_button');
-    if (element) {
-        element.onclick = onCandidateSearchButtonPressed;
+    let stateYear = document.getElementById('state-year');
+    let candidate = document.getElementById('candidate');
+    
+    if (stateYear && candidate) {
+        stateYear.onclick = onSearchByStateYearButtonPressed
+        candidate.onclick = onSearchByCandidateButtonPressed
     }
 }
 
@@ -22,6 +24,95 @@ function getAPIBaseURL() {
                     + ':' + window.location.port
                     + '/api';
     return baseURL;
+}
+
+function onSearchByStateYearButtonPressed() {
+    // Delete candidate search elements
+    let candidateSearchbox = document.getElementById('candidate-searchbox')
+    if (candidateSearchbox) {
+        candidateSearchbox.innerHTML = '';
+    }
+    let candidatesData = document.getElementById('candidate-election-history')
+    if (candidatesData) {
+        candidatesData.innerHTML = '';
+    }
+
+    // Handle states
+    let url = getAPIBaseURL()+ '/states/';
+    fetch(url, {method: 'get'})
+    .then((response) => response.json())
+    .then(function(states) {
+        let state_year_selector = '';
+        state_year_selector = '<p class="state-drop-down"><select id="state_selector">' +
+                        '<option value="' + String(0) + '">' +
+                        '--SELECT STATE--' + '</option>\n';
+        for (let i = 0; i < states.length; i++) {
+            let state = states[i];
+            state_year_selector += '<option value="' + state['state_id'] + '">'
+                            + state['state_name'] + '</option>\n';
+        }
+
+        state_year_selector += '</select></p>';
+        
+        // Handle years
+        state_year_selector += '<p class="year-drop-down"><select id="year_selector">' +
+                                '<option value="' + String(0) + '">' +
+                                '--SELECT YEAR--' + '</option>\n';
+        for (let i = 2020; i > 1975; i=i-2) {
+            state_year_selector += '<option value="' + String(i) + '">'
+                            + String(i) + '</option>\n';
+        }
+
+        state_year_selector += '</select></p>';
+
+        // "GO" button
+        state_year_selector += `<p class="go"><button><a class="button" id="search_button">
+                                GO</a></button></p>`
+
+        let selectorOnPage = document.getElementById('state-year-drop-down');
+        if (selectorOnPage) {
+            selectorOnPage.innerHTML = state_year_selector;
+        }
+
+        let searchButton = document.getElementById('search_button');
+        if (searchButton) {
+            searchButton.onclick = onStateYearSearchButtonPressed;
+        }
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+}
+
+function onStateYearSearchButtonPressed() {
+    ;
+}
+
+function onSearchByCandidateButtonPressed() {
+    // Delete state year elements
+    let dropDowns = document.getElementById('state-year-drop-down')
+    if (dropDowns) {
+        dropDowns.innerHTML = '';
+    }
+    let pieChartData = document.getElementById('pie-chart-data')
+    if (pieChartData) {
+        pieChartData.innerHTML = '';
+    }
+
+    let url = getAPIBaseURL() + '/search-by-candidate/';
+    let searchBox = `<input type="text" id="input_text" placeholder="Search by cadidate name">
+    <button><a class="button" id="search_button">SEARCH</a></button>`
+    
+    let searchBoxOnPage = document.getElementById('candidate-searchbox')
+    if (searchBoxOnPage) {
+        searchBoxOnPage.innerHTML = searchBox;
+    }
+
+    let searchButton = document.getElementById('search_button');
+    if (searchButton) {
+        searchButton.onclick = onCandidateSearchButtonPressed;
+    }
+
 }
 
 function onCandidateSearchButtonPressed() {
@@ -62,7 +153,7 @@ function onCandidateSearchButtonPressed() {
                 startingYear = election['year'];
             }
 
-            // Handeling other candidates
+            // Handling other candidates
             displayBody += election['other_candidate_name'] 
                             + ' - ' + 'Votes Received: ' 
                             + String(election['other_candidate_votes_received']) + '<br>';
@@ -70,8 +161,7 @@ function onCandidateSearchButtonPressed() {
 
         displayBody += '</p>'
 
-        // Put the table body we just built inside the table that's already on the page.
-        let electionHistoryOnPage = document.getElementById('candidate_election_history');
+        let electionHistoryOnPage = document.getElementById('candidate-election-history');
         if (electionHistoryOnPage) {
             electionHistoryOnPage.innerHTML = displayBody;
         }
